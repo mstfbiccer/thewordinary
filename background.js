59b59104b5e57ef8b6ad5deb18679fca0e7cb337
mstfbiccer <mstfbiccer@gmail.com>
2017-01-18 22:54:31 +0300

    updated

--- background.js
+++ background.js
@@ -26,9 +26,16 @@ var contextMenu = function(e) {
         // The user selected some text, put this in the message.
     jQuery.get("https://translate.yandex.net/api/v1.5/tr.json/detect?key=trnsl.1.1.20170115T215035Z.2067705a21a23db0.e9644dc2a0da269d204df1c2009d0599159f5433&text="+encodeURI(e.selectionText), function( data ) {   
         var lang=data['lang'];
-        jQuery.get("https://translate.yandex.net/api/v1.5/tr/translate?key=trnsl.1.1.20170115T215035Z.2067705a21a23db0.e9644dc2a0da269d204df1c2009d0599159f5433&text="+encodeURI(e.selectionText)+"&lang="+lang+"-tr", function( data2 ) {
-          gonder("Translate : "+lang+"-tr"+"\n"+e.selectionText+" : "+jQuery(data2).find('text').text());
-        });
+        if(lang!="tr") {
+            jQuery.get("https://translate.yandex.net/api/v1.5/tr/translate?key=trnsl.1.1.20170115T215035Z.2067705a21a23db0.e9644dc2a0da269d204df1c2009d0599159f5433&text="+encodeURI(e.selectionText)+"&lang="+lang+"-tr", function( data2 ) {
+              gonder("Translate : "+lang+"-tr"+"\n"+e.selectionText+" : "+jQuery(data2).find('text').text());
+            }); 
+        }else {
+            jQuery.get("https://translate.yandex.net/api/v1.5/tr/translate?key=trnsl.1.1.20170115T215035Z.2067705a21a23db0.e9644dc2a0da269d204df1c2009d0599159f5433&text="+encodeURI(e.selectionText)+"&lang="+lang+"-en", function( data2 ) {
+              gonder("Translate : "+lang+"-en"+"\n"+e.selectionText+" : "+jQuery(data2).find('text').text());
+            });
+        }
+
     });
     }
 };
