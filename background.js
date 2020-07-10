6913a21e02aedd7dcf15a6f55179ad1462657c0b
mustafabicer <mustafa.bicer@segmentify.com>
2020-07-11 02:22:06 +0300

    more detail button added

--- background.js
+++ background.js
@@ -2,13 +2,20 @@ window.Notification = (function () {
   var notification = null;
 
   return {
-    display: function (opt) {
-      try {
-        notification = chrome.notifications.create(opt);
-        notification.show();
-      } catch (err) {}
-
-
+    display: function (frmURL, opt) {
+      var notifId = ""
+      chrome.notifications.create(frmURL, opt, function (notificationId) {
+        notifId = notificationId
+      });
+      // create a on Click listener for notifications
+      /* Respond to the user's clicking one of the buttons */
+      var open_editor_in_tab = function (notifId, btnIdx) {
+        if (btnIdx === 0) {
+          chrome.notifications.onButtonClicked.removeListener(open_editor_in_tab);
+          window.open(frmURL);
+        }
+      }
+      chrome.notifications.onButtonClicked.addListener(open_editor_in_tab);
     },
     hide: function () {
       notification.close();
@@ -17,14 +24,16 @@ window.Notification = (function () {
 })();
 
 function gonder(content) {
-
   var opt = {
     type: "basic",
     title: "Learn it !",
     message: content,
-    iconUrl: "thewordinary.png"
+    iconUrl: "thewordinary.png",
+    buttons: [{
+      title: "More Detail!"
+    }]
   };
-  Notification.display(opt);
+  Notification.display('https://mstfbiccer.github.io/thewordinary/?word=' + content.split(':')[0].trim(), opt);
 }
 
 setInterval(function () {
@@ -33,7 +42,6 @@ setInterval(function () {
     enTRtranslateWord(wordsArray[Math.round(Math.random() * wordsArray.length - 2)].title.$t);
   });
 }, 2000000);
-
 function enTRtranslateWord(word) {
   jQuery.get('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20170521T212252Z.33f08df3be1d1141.c3bd46ef5dfd991df280d61ddc015e625344982f&lang=en-tr&text=' + encodeURI(word), function (data) {
     var control = false;
@@ -43,7 +51,7 @@ function enTRtranslateWord(word) {
         gonder(word + " : " + item.tr[0].text);
       }
     })
-    if(!control) {
+    if (!control) {
       data.def.forEach((item) => {
         if (item.pos === "adverb") {
           control = true;
@@ -51,7 +59,7 @@ function enTRtranslateWord(word) {
         }
       })
     }
-    if(!control) {
+    if (!control) {
       data.def.forEach((item) => {
         if (item.pos === "verb") {
           control = true;
@@ -59,7 +67,7 @@ function enTRtranslateWord(word) {
         }
       })
     }
-    if(!control) {
+    if (!control) {
       data.def.forEach((item) => {
         if (item.pos === "adjective") {
           control = true;
@@ -67,7 +75,7 @@ function enTRtranslateWord(word) {
         }
       })
     }
-    if(!control) {
+    if (!control) {
       data.def.forEach((item) => {
         if (item.pos === "participle") {
           control = true;
@@ -75,7 +83,7 @@ function enTRtranslateWord(word) {
         }
       })
     }
-    if(!control) {
+    if (!control) {
       data.def.forEach((item) => {
         if (item.pos === "preposition") {
           control = true;
--- popup.js
+++ popup.js
@@ -2,9 +2,20 @@ window.Notification = (function () {
   var notification = null;
 
   return {
-    display: function (opt) {
-      notification = chrome.notifications.create(opt);
-      notification.show();
+    display: function (frmURL, opt) {
+      var notifId = ""
+      chrome.notifications.create(frmURL, opt, function (notificationId) {
+        notifId = notificationId
+      });
+      // create a on Click listener for notifications
+      /* Respond to the user's clicking one of the buttons */
+      var open_editor_in_tab = function (notifId, btnIdx) {
+        if (btnIdx === 0) {
+          chrome.notifications.onButtonClicked.removeListener(open_editor_in_tab);
+          window.open(frmURL);
+        }
+      }
+      chrome.notifications.onButtonClicked.addListener(open_editor_in_tab);
     },
     hide: function () {
       notification.close();
@@ -13,14 +24,16 @@ window.Notification = (function () {
 })();
 
 function gonder(content) {
-
   var opt = {
     type: "basic",
     title: "Learn it !",
     message: content,
-    iconUrl: "thewordinary.png"
+    iconUrl: "thewordinary.png",
+    buttons: [{
+      title: "More Detail!"
+    }]
   };
-  Notification.display(opt);
+  Notification.display('https://mstfbiccer.github.io/thewordinary/?word=' + content.split(':')[0].trim(), opt);
 }
 jQuery(document).ready(function () {
   jQuery('#content,#sel1').change(function () {
@@ -45,9 +58,9 @@ function TRENtranslate(word) {
         jQuery('#result').val(item.tr[0].text);
       }
     })
-    if(data.def.length === 0) {
+    if (data.def.length === 0) {
       jQuery('#result').val('no result');
-    } 
+    }
   })
 
 }
@@ -59,9 +72,9 @@ function enTRtranslate(word) {
         jQuery('#result').val(item.tr[0].text);
       }
     })
-    if(data.def.length === 0) {
+    if (data.def.length === 0) {
       jQuery('#result').val('no result');
-    } 
+    }
   })
 }
 
@@ -74,7 +87,7 @@ function enTRtranslateWord(word) {
         gonder(word + " : " + item.tr[0].text);
       }
     })
-    if(!control) {
+    if (!control) {
       data.def.forEach((item) => {
         if (item.pos === "adverb") {
           control = true;
@@ -82,7 +95,7 @@ function enTRtranslateWord(word) {
         }
       })
     }
-    if(!control) {
+    if (!control) {
       data.def.forEach((item) => {
         if (item.pos === "verb") {
           control = true;
@@ -90,7 +103,7 @@ function enTRtranslateWord(word) {
         }
       })
     }
-    if(!control) {
+    if (!control) {
       data.def.forEach((item) => {
         if (item.pos === "adjective") {
           control = true;
@@ -98,7 +111,7 @@ function enTRtranslateWord(word) {
         }
       })
     }
-    if(!control) {
+    if (!control) {
       data.def.forEach((item) => {
         if (item.pos === "participle") {
           control = true;
@@ -106,7 +119,7 @@ function enTRtranslateWord(word) {
         }
       })
     }
-    if(!control) {
+    if (!control) {
       data.def.forEach((item) => {
         if (item.pos === "preposition") {
           control = true;
